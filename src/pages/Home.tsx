c095e840c52fcdc33511bdba95d0338cb0004c55
gershomlapaix <nsengiyumv33@gmail.com>
2022-11-22 12:55:45 +0200

    some more designs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Navigation from "../components/Navigation";
-import myImage from "../../public/images/fitboy.png";
+
 export default function Home() {
   return (
     <div className="homeContainer">
@@ -16,6 +17,12 @@ export default function Home() {
             We help you to create a shape of your body and doing this stay
             healthy taking our fitness service
           </p>
+
+          <div className="homeContainer__welcome__sectionOne__quick-actions">
+            <button className="homeContainer__welcome__sectionOne__quick-actions--btn">Get Started</button>
+
+            <Link to={`/booking`} className="homeContainer__welcome__sectionOne__quick-actions--booking-link">Book your seat</Link>
+          </div>
         </div>
         <div className="homeContainer__welcome__sectionTwo">
           <img src={process.env.PUBLIC_URL + "/images/fitboy.png"} alt="" />
